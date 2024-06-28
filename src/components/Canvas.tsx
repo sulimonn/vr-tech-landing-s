7c5ae9137a9a21f1d0818bf4ab69b53a4dc00b4d
sulimonn <gsuli836@gmail.com>
2024-06-29 03:14:33 +0600

    config

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,15 +1,13 @@
 import { useRef, FC, useEffect, useState } from 'react';
 
+import Wave from './Wave';
 import { CanvasContext } from '../hooks/useCanvas';
 import useResponsiveSize from '../hooks/useResponsiveSize';
-import Wave from './Wave';
 
 const Canvas: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { width } = useResponsiveSize();
-  const [context, setContext] = useState<
-    CanvasRenderingContext2D | undefined
-  >();
+  const [context, setContext] = useState<CanvasRenderingContext2D | undefined>();
 
   useEffect(() => {
     const ctx = canvasRef?.current?.getContext('2d');
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import config from '../config/index.json';
 import Divider from './Divider';
+import config from '../config/index.json';
 
 const Product = () => {
   const { product } = config;
@@ -10,14 +10,9 @@ const Product = () => {
   return (
     <section className={`bg-background py-8`} id="product">
       <div className={`container max-w-5xl mx-auto m-8`}>
-        <h1
-          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
-        >
+        <h1 className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}>
           {product.title.split(' ').map((word, index) => (
-            <span
-              key={index}
-              className={index % 2 ? 'text-primary' : 'text-border'}
-            >
+            <span key={index} className={index % 2 ? 'text-primary' : 'text-border'}>
               {word}{' '}
             </span>
           ))}
@@ -25,34 +20,22 @@ const Product = () => {
         <Divider />
         <div className={`flex flex-wrap`}>
           <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-            <h3
-              className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-            >
+            <h3 className={`text-3xl text-gray-800 font-bold leading-none mb-3`}>
               {firstItem?.title}
             </h3>
             <p className={`text-gray-600`}>{firstItem?.description}</p>
           </div>
           <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={firstItem?.img}
-              alt={firstItem?.title}
-            />
+            <img className="h-6/6" src={firstItem?.img} alt={firstItem?.title} />
           </div>
         </div>
         <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
           <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={secondItem?.img}
-              alt={secondItem?.title}
-            />
+            <img className="h-6/6" src={secondItem?.img} alt={secondItem?.title} />
           </div>
           <div className={`w-full sm:w-1/2 p-6 mt-20`}>
             <div className={`align-middle`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
+              <h3 className={`text-3xl text-gray-800 font-bold leading-none mb-3`}>
                 {secondItem?.title}
               </h3>
               <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
